dbe656accec08aca0f74151f891673ac16b6ee87
Niteshsinghbisht03 <niteshsinghbisht003@gmail.com>
2024-10-13 18:54:43 +0530

    First commit

---
+++ client/src/data/lessons/lesson-0/lesson-0-data.js
@@ -0,0 +1,220 @@
+const lesson_0_data = {
+  lessonNumber: 0,
+  lessonPages: 'Genki I: p.20-35',
+  lessonTitle: 'Get Your Status',
+  lessonUrl: 'lesson-0',
+  lessonUnits: [
+    {
+      unitTitle: 'Check',
+      unitUrl: 'greetings',
+      unitContent: [
+        // { character: '', meanings: '' },
+        {
+          character: ['Good morning.' ,'Good morning (polite)','Good afternoon.','Good evening.'],
+          meanings: 'suprabhat',
+        },
+        {
+          character: ['Good morning.' ,'Good morning (polite)','Good afternoon.','Good evening.'],
+          meanings: 'Good morning. (polite)',
+        },
+        {
+          character: ['Good morning.' ,'Good morning (polite)','Good afternoon.','Good evening.'],
+            meanings: 'Good afternoon.',
+        },
+        {
+          character: ['Good morning.' ,'Good morning (polite)','Good afternoon.','Good evening.'],
+          meanings: 'Good evening.',
+        },
+        {
+          character: ['Good morning.' ,'Good morning (polite)','Good afternoon.','Good evening.'],
+          anings: 'Goodbye.',
+        },
+        {
+          character: ['Good morning.' ,'Good morning (polite)','Good afternoon.','Good evening.'],
+          
+          meanings: 'Good night. (polite)',
+        },
+        {
+          character: ['Good morning.' ,'Good morning (polite)','Good afternoon.','Good evening.'],
+              meanings: 'Thank you.',
+        },
+        {
+          character: ['Good morning.' ,'Good morning (polite)','Good afternoon.','Good evening.'],
+          meanings: 'Thank you. (polite)',
+        },
+        {
+          character: ['Good morning.' ,'Good morning (polite)','Good afternoon.','Good evening.'],
+                    meanings: "Excuse me.; I'm sorry.",
+        },
+        {
+          character: ['Good morning.' ,'Good morning (polite)','Good afternoon.','Good evening.'],
+          eanings: 'No.; Not at all.',
+        },
+        {
+          character:['Good morning.' ,'Good morning (polite)','Good afternoon.','Good evening.'],
+                meanings: "I'll go and come back.",
+        },
+        {
+          character: ['Good morning.' ,'Good morning (polite)','Good afternoon.','Good evening.'],
+               meanings: 'Please go and come back.',
+        },
+        {
+          character: ['Good morning.' ,'Good morning (polite)','Good afternoon.','Good evening.'],
+            meanings: "I'm home.",
+        },
+        {
+          character: ['Good morning.' ,'Good morning (polite)','Good afternoon.','Good evening.'],
+             meanings: 'Welcome home.',
+        },
+        {
+          character: ['Good morning.' ,'Good morning (polite)','Good afternoon.','Good evening.'],
+
+          meanings: 'meal (before eating)',
+         
+        },
+        {
+          character:['Good morning.' ,'Good morning (polite)','Good afternoon.','Good evening.'],
+         
+          meanings: 'Thank you for the meal. (after eating)',
+        },
+        {
+          character: ['Good morning.' ,'Good morning (polite)','Good afternoon.','Good evening.'],
+            meanings: 'How do you do?',
+        },
+        {
+          character:['Good morning.' ,'Good morning (polite)','Good afternoon.','Good evening.'],
+           meanings: 'I am...',
+        },
+        {
+          character: ['Good morning.' ,'Good morning (polite)','Good afternoon.','Good evening.'],
+                    meanings: 'Nice to meet you.',
+        },
+      ],
+    },
+    // {
+    //   unitTitle: 'Numbers',
+    //   unitUrl: 'numbers',
+    //   unitContent: [
+    //     {
+    //       character: 'ぜろ/れい',
+    //       meanings: '0',
+    //     },
+    //     {
+    //       character: 'いち',
+    //       meanings: '1',
+    //     },
+    //     {
+    //       character: 'に',
+    //       meanings: '2',
+    //     },
+    //     {
+    //       character: 'さん',
+    //       meanings: '3',
+    //     },
+    //     {
+    //       character: 'よん/し/(よ)',
+    //       meanings: '4',
+    //     },
+    //     {
+    //       character: 'ご',
+    //       meanings: '5',
+    //     },
+    //     {
+    //       character: 'ろく',
+    //       meanings: '6',
+    //     },
+    //     {
+    //       character: 'なな/しち',
+    //       meanings: '7',
+    //     },
+
+    //     {
+    //       character: 'はち',
+    //       meanings: '8',
+    //     },
+    //     {
+    //       character: 'きゅう/く',
+    //       meanings: '9',
+    //     },
+    //     {
+    //       character: 'じゅう',
+    //       meanings: '10',
+    //     },
+    //     {
+    //       character: 'じゅういち',
+    //       meanings: '11',
+    //     },
+    //     {
+    //       character: 'じゅうに',
+    //       meanings: '12',
+    //     },
+    //     {
+    //       character: 'じゅうさん',
+    //       meanings: '13',
+    //     },
+    //     {
+    //       character: 'じゅうよん/じゅうし',
+    //       meanings: '14',
+    //     },
+    //     {
+    //       character: 'じゅうご',
+    //       meanings: '15',
+    //     },
+    //     {
+    //       character: 'じゅうろく',
+    //       meanings: '16',
+    //     },
+    //     {
+    //       character: 'じゅうなな/じゅうしち',
+    //       meanings: '17',
+    //     },
+    //     {
+    //       character: 'じゅうはち',
+    //       meanings: '18',
+    //     },
+    //     {
+    //       character: 'じゅうきゅう/じゅうく',
+    //       meanings: '19',
+    //     },
+    //     {
+    //       character: 'にじゅう',
+    //       meanings: '20',
+    //     },
+    //     {
+    //       character: 'さんじゅう',
+    //       meanings: '30',
+    //     },
+    //     {
+    //       character: 'よんじゅう',
+    //       meanings: '40',
+    //     },
+    //     {
+    //       character: 'ごじゅう',
+    //       meanings: '50',
+    //     },
+    //     {
+    //       character: 'ろくじゅう',
+    //       meanings: '60',
+    //     },
+    //     {
+    //       character: 'ななじゅう',
+    //       meanings: '70',
+    //     },
+    //     {
+    //       character: 'はちじゅう',
+    //       meanings: '80',
+    //     },
+    //     {
+    //       character: 'きゅうじゅう',
+    //       meanings: '90',
+    //     },
+    //     {
+    //       character: 'ひゃく',
+    //       meanings: '100',
+    //     },
+    //   ],
+    // },
+  ],
+};
+
+export default lesson_0_data;
